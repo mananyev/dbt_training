1f3dac49c3d757427b28a15a5059f202aa583410
Mikhail Ananyev <ananyev.mikhail@gmail.com>
2022-07-31 14:40:21 +0000

    Added snapshots. NO incremental models!

---
+++ snapshots/snap_daily_revenue.sql
@@ -0,0 +1,24 @@
+{% snapshot daily_revenue_snapshot %}
+
+{{
+    config(
+      target_database='dbtworkshop',
+      target_schema='snapshots',
+      unique_key='order_date',
+
+      strategy='check',
+      check_cols=['daily_revenue',],
+    )
+}}
+
+select
+    order_date,
+    sum(amount) as daily_revenue
+
+from {{ ref('fct_orders') }}
+
+group by 1
+
+order by 1
+
+{% endsnapshot %}
