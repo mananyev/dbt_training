61dddd5089aac435dac033d3eb9760f5d5822165
Mikhail Ananyev <ananyev.mikhail@gmail.com>
2022-07-31 18:23:20 +0000

    CTE groupings and cosmetic cleanup.

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,35 +1,61 @@
 with
 
+-- Import CTEs
+orders as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+
+customers as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+
+payments as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+
+-- Logical CTEs
+p as (
+
+    select
+        orderid as order_id,
+        max(created) as payment_finalized_date,
+        sum(amount) / 100.0 as total_amount_paid
+
+    from payments
+
+    where status <> 'fail'
+
+    group by 1
+    
+),
+
+
 paid_orders as (
 
     select
-        orders.id as order_id,
-        orders.user_id	as customer_id,
-        orders.order_date as order_placed_at,
-        orders.status as order_status,
+        o.id as order_id,
+        o.user_id	as customer_id,
+        o.order_date as order_placed_at,
+        o.status as order_status,
         p.total_amount_paid,
         p.payment_finalized_date,
         c.first_name as customer_first_name,
         c.last_name as customer_last_name
 
-    from {{ source('jaffle_shop', 'orders') }} as orders
+    from orders o
 
-    left join (
-
-        select
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-
-        from {{ source('stripe', 'payement') }}
-
-        where status <> 'fail'
-
-        group by 1
-
-    ) p on orders.id = p.order_id
+    left join p on o.id = p.order_id
     
-    left join {{ source('jaffle_shop', 'customers') }} c on orders.user_id = c.id
+    left join customers c on o.user_id = c.id
 
 ),
 
@@ -40,51 +66,61 @@ customer_orders as (
         c.id as customer_id
         , min(order_date) as first_order_date
         , max(order_date) as most_recent_order_date
-        , count(orders.id) as number_of_orders
+        , count(o.id) as number_of_orders
     
-    from {{ source('jaffle_shop', 'customers') }} c 
+    from customers c 
 
-    left join {{ source('jaffle_shop', 'orders') }} as orders
-        on orders.user_id = c.id 
+    left join orders o
+        on o.user_id = c.id 
 
     group by 1
 
-)
+),
 
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (
-        partition by customer_id order by p.order_id
-    ) as customer_sales_seq,
-    case
-        when c.first_order_date = p.order_placed_at
-        then 'new'
-        else 'return'
-    end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
 
-from paid_orders p
-
-left join customer_orders as c using (customer_id)
-
-left outer join (
+x as (
 
     select
-        p.order_id,
+        po.order_id,
         sum(t2.total_amount_paid) as clv_bad
     
-    from paid_orders p
+    from paid_orders po
     
     left join paid_orders t2
-        on p.customer_id = t2.customer_id
-        and p.order_id >= t2.order_id
+        on po.customer_id = t2.customer_id
+        and po.order_id >= t2.order_id
     
     group by 1
     
-    order by p.order_id
+    order by po.order_id
 
-) x on x.order_id = p.order_id
+),
 
-order by order_id
+
+-- Final CTE
+final as (
+
+    select
+        po.*,
+        row_number() over (order by po.order_id) as transaction_seq,
+        row_number() over (
+            partition by customer_id order by po.order_id
+        ) as customer_sales_seq,
+        case
+            when c.first_order_date = po.order_placed_at
+            then 'new'
+            else 'return'
+        end as nvsr,
+        x.clv_bad as customer_lifetime_value,
+        c.first_order_date as fdos
+
+    from paid_orders po
+
+    left join customer_orders as c using (customer_id)
+
+    left outer join x on x.order_id = po.order_id
+
+    order by order_id
+)
+
+select * from final
