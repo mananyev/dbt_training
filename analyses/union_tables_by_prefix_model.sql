12ab9b11b6eb7d05d64bece751815456c350afd4
Mikhail Ananyev <ananyev.mikhail@gmail.com>
2022-07-31 15:05:57 +0000

    Added legacy model to an empty project.

--- analyses/union_tables_by_prefix_model.sql
+++
@@ -1,9 +0,0 @@
-{{ create_tables_amazon_shopify() }}
-
-{{
-    union_tables_by_prefix(
-        database=target.dbname,
-        schema='stripe',
-        prefix='orders__'
-    )
-}}
--- macros/limit_data_in_dev.sql
+++
@@ -1,8 +0,0 @@
-{#
-This is the code made to work in Redshift!
-#}
-{% macro limit_data_in_dev(column_name, dev_days_of_data) -%}
-{%- if target.name == 'default' -%}
-where {{ column_name }} >= current_date - interval '{{ dev_days_of_data }} days'
-{%- endif -%}
-{%- endmacro %}
